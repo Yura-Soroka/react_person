958763eca62c04454a478a648acf9c5413fe0195
Yura Soroka <ykotov18@gmail.com>
2025-05-16 14:06:41 +0300

    solution3

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,7 +2,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-      {person.age && typeof person.age === 'number' && (
+      {person.age !== undefined && typeof person.age === 'number' && (
         <p className="Person__age">{`I am ${person.age}`}</p>
       )}
       {!person.isMarried && <p className="Person__partner">I am not married</p>}
